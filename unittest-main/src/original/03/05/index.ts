9ea0f1caf1b4d60150cc1042442d20e5006f26ff
Kohta9521 <kohta0421kochi@gmail.com>
2023-08-31 00:08:18 +0900

    add toThrowError section

--- unittest-main/src/original/03/05/index.ts
+++ unittest-main/src/original/03/05/index.ts
@@ -1,13 +1,27 @@
-export function add(a: number, b:number) {
-  if( a < 0 || a > 100) {
-    throw new Error("aの入力値は0~100の間で入力してください。");
+export class RangeError extends Error {}
+
+function checkRange(value: number) {
+  if (value < 0 || value > 100) {
+    throw new RangeError("入力値は0 ~ 100の間で入力してください");
   }
-  if( b < 0 || b > 100) {
-    throw new Error("bの入力値は0~100の間で入力してください。");
-  }
+}
+
+export function add(a: number, b: number) {
+  checkRange(a);
+  checkRange(b);
   const sum: number = a + b;
-  if (sum > 100) {
+  if ( sum > 100) {
     return 100;
+  };
+  return sum;
+}
+
+export function sub(a: number, b: number) {
+  checkRange(a);
+  checkRange(b);
+  const sum = a - b;
+  if (sum < 0) {
+    return 0;
   }
-  return a =+ b;
+  return sum;
 }
